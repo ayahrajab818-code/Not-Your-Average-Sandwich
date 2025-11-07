92c981a9953d0c87818a3c813645e27b6d8bb459
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-07 14:45:11 -0500

    just got done making the SANDWICHSIZE class

---
+++ src/main/java/com/pluralsight/SandwichSize.java
@@ -0,0 +1,11 @@
+package com.pluralsight;
+/*
+ * This class stores sandwich sizes.
+ */
+public class SandwichSize {
+    public static String[] SIZES = {
+            "4",
+            "8",
+            "12"
+    };
+}

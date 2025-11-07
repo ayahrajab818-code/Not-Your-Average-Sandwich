c8d38403b27e7ee80ee7284133d8851645977724
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-07 14:43:15 -0500

    just got done making the BREAD class

--- src/main/java/com/pluralsight/Bread.java
+++ src/main/java/com/pluralsight/Bread.java
@@ -1,4 +1,12 @@
 package com.pluralsight;
-
+/*
+ * This class stores all bread options for sandwiches.
+ */
 public class Bread {
+    public static String[] TYPES = {
+            "White",
+            "Wheat",
+            "Rye",
+            "Wrap"
+    };
 }

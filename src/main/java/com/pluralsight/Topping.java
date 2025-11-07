8a7c4e8b38ad736722b8a0b8a73aac9b27ae900b
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-07 14:48:34 -0500

    Working on the topping class now

--- src/main/java/com/pluralsight/Topping.java
+++ src/main/java/com/pluralsight/Topping.java
@@ -1,4 +1,15 @@
 package com.pluralsight;
-
+/*
+ * This class stores toppings.
+ * Meat and cheese cost extra, regular toppings are free.
+ */
 public class Topping {
+    public static String[] MEATS = {
+            "Steak",
+            "Ham",
+            "Salami",
+            "Roast Beef",
+            "Chicken",
+            "Bacon"
+    };
 }

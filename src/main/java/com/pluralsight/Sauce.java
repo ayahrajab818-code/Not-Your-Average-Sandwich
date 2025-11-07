f39404c352a35aaa8844e21442dec43bf33d19df
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-07 14:46:51 -0500

    just got done making the SAUCES class

--- src/main/java/com/pluralsight/Sauce.java
+++ src/main/java/com/pluralsight/Sauce.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
-
+/*
+ * This class stores all sauce options.
+ */
 public class Sauce {
+    public static String[] SAUCES = {
+            "Mayo",
+            "Mustard",
+            "Ketchup",
+            "Ranch",
+            "Thousand Islands",
+            "Vinaigrette"
+    };
 }

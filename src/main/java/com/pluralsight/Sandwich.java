1d0705dc422db40f5c39d43cd8d90fd907f34100
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-07 15:57:03 -0500

    working

--- src/main/java/com/pluralsight/Sandwich.java
+++ src/main/java/com/pluralsight/Sandwich.java
@@ -1,7 +1,29 @@
 package com.pluralsight;
 
 
-public class Sandwich {
+import java.util.ArrayList;
 
+public class Sandwich extends Product{
+    private String bread; //Bread type
+    private String size; //Sandwich size
+    private boolean toasted; //Toasted or not
+    private ArrayList<ToppingItem> t = new ArrayList<>(); // List of toppings
+
+    public Sandwich(String bread, String size, boolean toasted, ArrayList<ToppingItem> t) {
+        this.bread = bread;
+        this.size = size;
+        this.toasted = toasted;
+        this.t = t;
+    }
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
 }
 

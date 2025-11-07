dbc165ea7cc2ca56873f206cdc31a188a0400259
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-07 15:52:06 -0500

    working

--- src/main/java/com/pluralsight/ToppingItem.java
+++ src/main/java/com/pluralsight/ToppingItem.java
@@ -1,4 +1,37 @@
 package com.pluralsight;
 
 public class ToppingItem {
+    private String name;
+    private String type; //Topping type: MEAT, CHEESE, REGULAR, SAUCE
+    private boolean extra; //True if customer wants extra amount
+
+    public ToppingItem(String name, String type, boolean extra) {
+        this.name = name;
+        this.type = type;
+        this.extra = extra;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isExtra() {
+        return extra;
+    }
+
+    public void setExtra(boolean extra) {
+        this.extra = extra;
+    }
 }

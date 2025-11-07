1f13dad8b7a76a404c9fb2927d177ac70436026f
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-07 14:54:00 -0500

    working on product class

--- src/main/java/com/pluralsight/Product.java
+++ src/main/java/com/pluralsight/Product.java
@@ -1,5 +1,13 @@
 package com.pluralsight;
-
+/*
+ * Product is a parent class for all items (Sandwich, Drink, Chips).
+ * Abstract = cannot create a Product directly, only subclasses.
+ */
 public abstract class Product {
 
+    //Every product must return its price
+    public abstract double getPrice();
+
+    //Every product must return a description for receipts
+    public abstract String getDescription();
 }

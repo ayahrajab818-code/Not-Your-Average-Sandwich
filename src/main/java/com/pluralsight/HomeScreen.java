7ccce5a8b3f81002fca461290fbf91a3e6c51640
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-09 00:01:24 -0500

    working

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -4,9 +4,7 @@ package com.pluralsight;
 public class HomeScreen {
     public static void main(String[] args) {
 
-        while(true){
-
-            String mainMenu = """
+            String homeMenu = """
             //------------Main Menu------------//
             ========================
             What do you want to do?
@@ -14,6 +12,14 @@ public class HomeScreen {
             0) Exit
             =======================
             """;
+
+            while (true) {
+            System.out.println(homeMenu);
+            String command = ConsoleHelper.promptForString("Enter command (N, X): ").toUpperCase();
+
+            switch (command){
+                
+            }
         }
     }
 }

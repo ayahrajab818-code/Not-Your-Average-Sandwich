8fd4d4536bc1a55bfe2cbf8042f55bb04452f72e
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-09 18:19:42 -0500

    working

--- src/main/java/com/pluralsight/BLT.java
+++ src/main/java/com/pluralsight/BLT.java
@@ -6,6 +6,11 @@ public class BLT extends SignatureSandwich {
     public BLT(){
         super("white", "8",true);
 
+        //Add default toppings
         addTopping(new ToppingItem("Bacon", "MEAT", false));
+        addTopping(new ToppingItem("Cheddar", "CHEESE", false));
+        addTopping(new ToppingItem("Lettuce", "REGULAR", false));
+        addTopping(new ToppingItem("Tomato", "REGULAR", false));
+        addTopping(new ToppingItem("Ranch", "SAUCE", false));
     }
 }

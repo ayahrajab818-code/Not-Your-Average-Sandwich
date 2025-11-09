e0ae5d8272590bc29d6e48a383f0d04a4b769fdf
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-08 22:07:10 -0500

    working

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,5 +1,15 @@
 package com.pluralsight;
 
-public class Chips {
+public class Chips extends Product{
 
+
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
 }

fe31d41b5e0a16ab64c4a7b9c7a51b76781d1fc3
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-08 23:02:45 -0500

    working

---
+++ src/main/java/com/pluralsight/ReceiptFile.java
@@ -0,0 +1,13 @@
+package com.pluralsight;
+
+
+import java.time.LocalDateTime;
+
+public class ReceiptFile {
+    public static void writeReceipt(Order o){
+        try{
+            LocalDateTime now = LocalDateTime.now(); //Get current date/time
+        }
+    }
+
+}

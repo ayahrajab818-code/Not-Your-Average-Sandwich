1abc9ad756082375911b44eab69ca12dea02fe0f
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-08 23:10:38 -0500

    working

--- src/main/java/com/pluralsight/ReceiptFile.java
+++ src/main/java/com/pluralsight/ReceiptFile.java
@@ -2,11 +2,16 @@ package com.pluralsight;
 
 
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ReceiptFile {
     public static void writeReceipt(Order o){
         try{
             LocalDateTime now = LocalDateTime.now(); //Get current date/time
+
+            //File name format yyyyMMdd-HHmmss.txt
+            DateTimeFormatter fileFormat = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+
         }
     }
 

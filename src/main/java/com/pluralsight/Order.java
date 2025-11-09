78080d9a36e38b98ca41c6ad0fa7bea7ded1c8cc
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-08 22:47:30 -0500

    working

--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -6,7 +6,10 @@ import java.util.ArrayList;
 public class Order {
     private ArrayList<Product> items = new ArrayList<>(); //List of products
 
-
+    //Add a product to the order
+    public void add(Product p) {
+        items.add(p);
+    }
 
 
 

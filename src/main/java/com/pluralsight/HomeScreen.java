db9da254e74a63d8b9bc9c27a4de9443ded862c2
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-09 00:15:06 -0500

    working

--- src/main/java/com/pluralsight/HomeScreen.java
+++ src/main/java/com/pluralsight/HomeScreen.java
@@ -4,20 +4,20 @@ package com.pluralsight;
 public class HomeScreen {
     public static void main(String[] args) {
 
-            String homeMenu = """
-            //------------Main Menu------------//
-            ========================
-            What do you want to do?
-            1) New Order
-            0) Exit
-            =======================
-            """;
+        String homeMenu = """
+                //------------Main Menu------------//
+                ========================
+                What do you want to do?
+                1) New Order
+                0) Exit
+                =======================
+                """;
 
-            while (true) {
+        while (true) {
             System.out.println(homeMenu);
             String command = ConsoleHelper.promptForString("Enter command (N, X): ").toUpperCase();
 
-            switch (command){
+            switch (command) {
 
                 //Start new order
                 case "N" -> {
@@ -25,19 +25,33 @@ public class HomeScreen {
                     boolean running = true;
 
                     String orderMenu = """
-                    //------------Order Menu------------//
-                    ========================
-                    What would you like to add?
-                    S) Add Sandwich
-                    D) Add Drink
-                    C) Add Chips
-                    O) Checkout
-                    X) Cancel Order
-                    =======================
-                    """;
+                            //------------Order Menu------------//
+                            ========================
+                            What would you like to add?
+                            S) Add Sandwich
+                            D) Add Drink
+                            C) Add Chips
+                            O) Checkout
+                            X) Cancel Order
+                            =======================
+                            """;
 
-                    while(running){
+                    while (running) {
                         System.out.println(orderMenu);
+                        String c = ConsoleHelper.promptForString("Enter command (S, D, C, O, X): ").toUpperCase();
+
+                        switch (c) {
+                            case "S" -> order.add(addSandwich());
+                            case "D" -> order.add(addDrink());
+                            case "C" -> order.add(addChips());
+                            case "O" -> {
+                                checkout(order);
+                                running = false;
+                            }
+                            
+
+
+                        }
                     }
                 }
             }

bbb8a9969a0389b0a4c3ca63c65ce6314ad3e3d6
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-08 22:26:17 -0500

    working

--- src/main/java/com/pluralsight/Chips.java
+++ src/main/java/com/pluralsight/Chips.java
@@ -1,7 +1,11 @@
 package com.pluralsight;
 
 public class Chips extends Product{
+    private String flavor; //Flavor of chips
 
+    public Chips(String flavor) {
+        this.flavor = flavor;
+    }
 
     @Override
     public double getPrice() {

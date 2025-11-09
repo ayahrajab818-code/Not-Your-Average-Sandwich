78353334edf8ea7ac775af5d66f655392edeef99
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-08 22:07:50 -0500

    working

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,5 +1,14 @@
 package com.pluralsight;
 
-public class Drink {
+public class Drink extends Product{
 
+    @Override
+    public double getPrice() {
+        return 0;
+    }
+
+    @Override
+    public String getDescription() {
+        return "";
+    }
 }

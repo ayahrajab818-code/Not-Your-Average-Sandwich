7258e2c91c84834c9414aa7d81df10472e5d44c0
ayahrajab818-code <ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.comgit config --global user.email ayahrajab818@gmail.com>
2025-11-09 18:24:39 -0500

    working

--- src/main/java/com/pluralsight/PhillyCheeseSteak.java
+++ src/main/java/com/pluralsight/PhillyCheeseSteak.java
@@ -1,4 +1,14 @@
 package com.pluralsight;
 
-public class PhillyCheeseSteak {
+import com.pluralsight.models.ToppingItem;
+
+public class PhillyCheeseSteak extends SignatureSandwich{
+    public PhillyCheeseSteak(){
+        super("white","8", true);
+
+        addTopping(new ToppingItem("Steak", "MEAT", false));
+        addTopping(new ToppingItem("American", "CHEESE", false));
+        addTopping(new ToppingItem("Peppers", "REGULAR", false));
+        addTopping(new ToppingItem("Mayo", "SAUCE", false));
+    }
 }
